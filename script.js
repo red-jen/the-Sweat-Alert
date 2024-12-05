4e9abc8c0e6d331df978ac702d069f72dcb91047
red-jen <redaje102105@gmail.com>
2024-12-05 14:23:41 +0100

    remove aler timeout

--- script.js
+++ script.js
@@ -13,28 +13,41 @@ let buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-      const type = button.getAttribute('data-type');
-
-      console.log(type);
-      
-     const  alert = document.createElement('div');
-
-    //   alert.classList.add('alert');
-      alert.classList.add(`${type}`);
-
-      alert.innerHTML=`
-                       <h1>${titles[type]}</h1>
-                     
-      `;
-
-
-
-      document.body.before(alert);
-      
-    });
+        const type = button.getAttribute('data-type');
+        creatAlert(type);
+   
+     });
   });
 
+  function creatAlert(type){
+  
 
+    console.log(type);
+    
+   const  alert = document.createElement('div');
+
+  //   alert.classList.add('alert');
+  setTimeout(function (){ 
+    alert.classList.add(`${type}`);
+
+    alert.innerHTML=`
+                     <h1>${titles[type]}</h1>
+                   
+    `;
+  }, 300)
+
+  setTimeout(() => {
+    alert.classList.add(`remove`);
+    setTimeout(() => {
+        alert.remove();
+    }, 300);
+    
+  }, 3000);
+
+
+
+    document.body.before(alert);
+}
 
 
 
